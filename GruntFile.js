8c8f07a302a5ad1b3ce9eee2e8c2bf4e189289fa
lijinxin <smelrain@gmail.com>
2015-01-22 23:02:21 +0800

    'new-built'

--- GruntFile.js
+++ GruntFile.js
@@ -6,15 +6,12 @@
 module.exports = function (grunt) {
     //加载辅助模块
     var path = require('path'),
-        fs = require('fs'),
         _ = require('underscore');
+
     //读取配置文件
     var gConfig = grunt.file.readJSON('grunt.config.json'),
         paths = gConfig.path,
         depth = gConfig.depth;
-    console.log(_.map(paths, function (path) {
-        return path;
-    }));
 
     //init
     grunt.initConfig({
@@ -27,6 +24,55 @@ module.exports = function (grunt) {
             })
         }
     });
+
+    //所有的插件配置
+    var extensions = function (type, src) {
+        return {
+            jshint: {  //js语法检测
+                options: {
+                    globals : {
+                        jQuery: true,
+                        console: true,
+                        module: true
+                    }
+                },
+                files : [src]
+            },
+            uglify : {   //压缩js
+                options : {
+                    banner : '/*' + src + 'was created by' + grunt.template.today('yyyy-mm-dd') + 'by redrock' + '*/'
+                },
+                build : {
+                    files : [{
+                        expand : true,
+                        dest : 'public/js/',
+                        src : src,
+                        rename : changeDest
+                    }]
+                }
+            },
+            cssmin : {  //压缩css
+                build : {
+                    files : [{
+                        expand : true,
+                        src : src,
+                        dest : 'public/css/',
+                        rename : changeDest
+                    }]
+                }
+            },
+            imagemin : {   //压缩图片
+                static : {
+                    files : [{
+                        expand : true,
+                        src : src,
+                        dest : 'public/images/',
+                        rename : changeDest
+                    }]
+                }
+            }
+        };
+    };
 
     //依赖包
     var devNpms = [
@@ -42,7 +88,7 @@ module.exports = function (grunt) {
     //加载依赖包
     (function () {
         devNpms.forEach(function (npm) {
-            grunt.loadNpmTasks(npm)
+            grunt.loadNpmTasks(npm);
         });
     })();
 
@@ -50,7 +96,58 @@ module.exports = function (grunt) {
     grunt.registerTask('default', 'watch');
 
     //watch事件处理文件
-    grunt.event.on('watch', function(type, src){
-        console.log(src + ':' + type );
+    grunt.event.on('watch', function (type, src) {
+            var isDir = grunt.file.isDir(src),
+                isFile = grunt.file.isFile(src),
+                tasks = [],
+                queenTasks = grunt.config.data;
+        console.log(isFile);
+        console.log(src);
+            if (isDir) {
+
+            } else if (isFile) {
+                var ext = path.extname(src),
+                    isJs = ['.js', '.json'].indexOf(ext) != -1,
+                    isCss = '.css' === ext,
+                    isImg = ['.png', '.gif', '.jpg'].indexOf(ext) != -1;
+                console.log(isImg);
+
+                if (isJs) {
+                    queenTasks.jshint = extensions(type, src).jshint;
+                    queenTasks.uglify = extensions(type, src).uglify;
+                    tasks.push('jshint', 'uglify');
+                }
+
+                if(isCss){
+                    queenTasks.cssmin = extensions(type, src).cssmin;
+                    tasks.push('cssmin');
+                }
+
+                if(isImg){
+                    queenTasks.imagemin = extensions(type, src).imagemin;
+                    tasks.push('imagemin');
+                    console.log('hahh');
+                }
+            }
+
+        grunt.task.run(tasks);
     });
+
+    /*------- 辅助函数-----*/
+    //@desc 修改文件路径
+    //@param type,src
+    //return dest
+    function changeDest(dest, src){
+        var count = depth, //深度
+            iIndex;
+        console.log(src);
+
+        while(count--){
+            iIndex = src.indexOf('/');
+            src = src.slice(iIndex+1);
+        }
+
+        return dest + src;
+    }
+    console.log('dsfuguy');
 };
