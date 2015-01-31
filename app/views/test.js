9280460c0d9405e1ee77718a6e32428ff4730b26
lijinxin <smelrain@gmail.com>
2015-01-31 11:20:56 +0800

    'grunt-build'

--- app/views/test.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Rain on 2015/1/31.
- */
-var name = 'lijinxin';
-var age = 20;v
-var sex = 'm';
--- public/js/test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Rain on 2015/1/31.
- */
-var name = "lijinxin", age = 20;
-
-v;
-
-var sex = "m";
