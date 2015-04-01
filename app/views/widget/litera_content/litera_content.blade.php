d1da1e115a924b3cf1d56c9e5b58ca0d0c5b4343
Lich <longzy2013@gmail.com>
2015-04-01 16:58:18 +0800

    add data

--- app/views/widget/litera_content/litera_content.blade.php
+++ app/views/widget/litera_content/litera_content.blade.php
@@ -1,5 +1,5 @@
 {{--文章详细页--}}
-<article class="m-litera_conent f-cb">
+<article class="m-litera_conent f-cb" data-value="{{$data['passage']['user']['id']}}">
     {{--文章头部--}}
     <div class="u-litera_content_h">
         <h5 class="js-share_title">{{$data['passage']['title']}}</h5>
--- app/views/widget/microm_list/microm_list.blade.php
+++ app/views/widget/microm_list/microm_list.blade.php
@@ -2,7 +2,7 @@
 <div class="m-movie_lists">
     @foreach($data['micromovielist'] as $micromovie)
         <div class="u-movie_item f-cb">
-            <img src="" alt="" class="u-m_show_img"/>
+            <img src="{{$micromovie['cover_url']}}" alt="" class="u-m_show_img"/>
             <aside class="u-m_info">
                 <a href="{{route('microm_sub')}}?type_id={{$micromovie['type_id']}}&passage_id={{$micromovie['id']}}" class="s-link_wrap">
                     <div class="u-m_info_h f-cb">
