bd4c185c89998f90a9a18857febcd60a00cd9008
Lich <longzy2013@gmail.com>
2015-04-05 20:02:21 +0800

    personal link

--- app/controllers/PersonalController.php
+++ app/controllers/PersonalController.php
@@ -36,6 +36,7 @@ class PersonalController extends BaseController {
             'from' => $from,
             'to' => $to,
         );
+//        return $works;
         return View::make('template.personal.personal')->with('data', $data);//TODO:需要一个高效的方法来取数据, 文章/图片/视频, 思考.
 	}
 
