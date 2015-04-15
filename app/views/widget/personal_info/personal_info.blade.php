4978a6623f3ec2e4bd2b7f566bbee75939ae5f7a
lijinxin <smelrain@gmail.com>
2015-04-15 16:11:16 +0800

    'change'

--- app/views/widget/personal_info/personal_info.blade.php
+++ app/views/widget/personal_info/personal_info.blade.php
@@ -4,10 +4,10 @@
         <div class="m-info_detail">
             <img src="{{$data['user_info']['head']}}" alt="" class="u-user_show_icon js-header_icon"/>
             <h5 class="js-user_name">{{$data['user_info']['username']}}</h5>
+             <ul class="u-user_extra_info">
+                            <li><span>E-mail:</span><span>{{$data['user_info']['email']}}</span></li>
+             </ul>
             <h6 class="js-user_signatrue">{{$data['user_info']['introduce']}}</h6>
-            <ul class="u-user_extra_info">
-                <li><span>E-mail:</span><span>{{$data['user_info']['email']}}</span></li>
-            </ul>
         </div>
     </div>
     <div class="m-change_info js-change_box">
