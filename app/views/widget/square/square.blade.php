aff9b4f854f43a03a2b1a09f2225d44e668a6bac
Lich <longzy2013@gmail.com>
2015-04-27 21:52:16 +0800

    ok

--- app/views/widget/square/square.blade.php
+++ app/views/widget/square/square.blade.php
@@ -3,54 +3,54 @@
     <div class="u-logo"></div>
     <ul class="u-squares">
         <li class="u-square_item">
-            <a href="jsns/literature" class="s-wrap"></a>
+            <a href="{{route('literature')}}" class="s-wrap"></a>
             <p class="u-title">
-                <a href="jsns/literature">
+                <a href="{{route('literature')}}">
                     <span>文学</span>
                     <span>literature</span>
                 </a>
             </p>
         </li>
         <li class="u-square_item">
-            <a href="jsns/photos" class="s-wrap"></a>
+            <a href="{{route('photos')}}" class="s-wrap"></a>
             <p class="u-title">
-                <a href="jsns/photos">
+                <a href="{{route('photos')}}">
                     <span>爱拍</span>
                     <span>photograph</span>
                 </a>
             </p>
         </li>
         <li class="u-square_item">
-            <a href="jsns/bookmovie" class="s-wrap"></a>
+            <a href="{{route('recommend')}}" class="s-wrap"></a>
             <p class="u-title">
-                <a href="jsns/bookmovie">
+                <a href="{{route('recommend')}}">
                     <span>读书·影逝</span>
                     <span>read&movie</span>
                 </a>
             </p>
         </li>
         <li class="u-square_item">
-            <a href="jsns/micromovie" class="s-wrap"></a>
+            <a href="{{route('micromovie')}}" class="s-wrap"></a>
             <p class="u-title">
-                <a href="jsns/micromovie">
+                <a href="{{route('micromovie')}}">
                     <span>微视</span>
                     <span>micro film</span>
                 </a>
             </p>
         </li>
         <li class="u-square_item">
-            <a href="jsns/original" class="s-wrap"></a>
+            <a href="{{route('original')}}" class="s-wrap"></a>
             <p class="u-title">
-                <a href="jsns/original">
+                <a href="{{route('original')}}">
                     <span>原味</span>
                     <span>original</span>
                 </a>
             </p>
         </li>
         <li class="u-square_item">
-            <a href="jsns/personal" class="s-wrap"></a>
+            <a href="{{route('personal')}}" class="s-wrap"></a>
             <p class="u-title">
-                <a href="jsns/personal">
+                <a href="{{route('personal')}}">
                     <span>个人中心</span>
                     <span>personal</span>
                 </a>
