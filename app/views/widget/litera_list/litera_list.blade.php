dfccb0fe79d306990117a8b167d8aea0677d8e20
lijinxin <smelrain@gmail.com>
2015-04-28 21:30:46 +0800

    'push'

--- app/views/widget/litera_list/litera_list.blade.php
+++ app/views/widget/litera_list/litera_list.blade.php
@@ -9,9 +9,9 @@
                     <h5>{{$value['title']}}</h5>
                     <span>{{$value['user']['username']}}</span>
                 </div>
-                <p class="u-content-omit">
+                <div class="u-content-omit">
                     <a class="js-stop_copy" href="litera_sub?passage_id={{$value['id']}}&type_id={{$value['type_id']}}">
-                       <div>{{str_limit($value['content'], '250', '......')}}</div>
+                        {{str_limit($value['content'], '250', '......')}}
                     </a>
                     <div class="u-litera_user_action f-cb">
                         <div class="show_litera-comments">
@@ -21,7 +21,7 @@
                         </div>
                         <div class="show_litera_love js-praise" data-type_id="{{$value['type_id']}}" data-passage_id="{{$value['id']}}"><i class="fa fa-heart js-show_love"></i><span class="js-num">({{$value['love_num']}})</span></div>
                     </div>
-                </p>
+                </div>
             </article>
         </div>
     @endforeach
