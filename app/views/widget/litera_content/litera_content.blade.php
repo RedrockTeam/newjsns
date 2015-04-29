272e835d9e538de78e28ca63bb9a602ea5e1555e
lijinxin <smelrain@gmail.com>
2015-04-29 23:02:16 +0800

    'fix'

--- app/views/widget/litera_content/litera_content.blade.php
+++ app/views/widget/litera_content/litera_content.blade.php
@@ -1,5 +1,5 @@
 {{--文章详细页--}}
-<article class="m-litera_conent f-cb js-content_info" data-value="{{$data['passage']['user']['id']}}">
+<div class="m-litera_conent f-cb js-content_info" data-value="{{$data['passage']['user']['id']}}">
     {{--文章头部--}}
     <div class="u-litera_content_h">
         <h5 class="js-share_title">{{$data['passage']['title']}}</h5>
@@ -7,12 +7,12 @@
         <span class="js-content_title">{{$data['passage']['user']['username']}}</span>
     </div>
     {{--文章内容--}}
-    <div class="u-litera_content js-share_content js-stop_copy">
+    <article class="u-litera_content js-share_content js-stop_copy">
         {{$data['passage']['content']}}
-    </div>
+    </article>
     {{--文章时间--}}
     <div class="u-article_date">{{$data['passage']['created_at']}}</div>
-</article>
+</div>
 @section("css")
     @parent
     {{HTML::style("public/css/widget/litera_content/litera_content.css")}}
