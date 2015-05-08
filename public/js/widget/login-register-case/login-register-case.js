037f9a696b1e8b7653b976a88665409f1e238119
lijinxin <840277005@qq.com>
2015-05-08 23:54:38 +0800

    'hhh'

--- public/js/widget/login-register-case/login-register-case.js
+++ public/js/widget/login-register-case/login-register-case.js
@@ -1,1 +1,39 @@
-define(["jquery","port"],function(B,A){B(function(){function C(F,G){F.on("click",function(){var H=B(this);H.hasClass("s-active")||(B(".u-tabs li").removeClass("s-active"),B(".u-register,.u-login").css("display","none"),H.addClass("s-active"),G.css("display","block"))})}function D(H){var G=B(this),F={stu_id:/^[\d]{7}$/gi,stu_pwd:/^[\w]{6,20}$/g,stu_nickname:/^([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[\w])+$/gi};E.call(G,F)||H.preventDefault()}function E(G){var H=B(this),F=!0,I=B(".js-check",H);return I.each(function(K,M){var L=B(M),J=L.attr("data-check_type");G[J]&&"[object RegExp]"==Object.prototype.toString.call(G[J])&&(G[J].test(L.val())?L.next(".s-error").css("display","none"):(L.next(".s-error").css("display","block"),F=!1))}),console.log(F),F}B(".m-login-register-case").on("click",function(F){F.stopPropagation()}),C(B(".js-open-register"),B(".u-register")),C(B(".js-open-login"),B(".u-login")),B(".js-login-form, .js-register-form").on("submit",D)})});
+/**
+ * Created by redrock on 2015/3/13.
+ */
+define([ "jquery", "port" ], function($, port) {
+    $(function() {
+        /*-------event 处理函数-----*/
+        function openBox($openEle, $openBox) {
+            //tab切换
+            $openEle.on("click", function() {
+                var $self = $(this);
+                $self.hasClass("s-active") || ($(".u-tabs li").removeClass("s-active"), $(".u-register,.u-login").css("display", "none"), 
+                $self.addClass("s-active"), $openBox.css("display", "block"));
+            });
+        }
+        function checkForm(ev) {
+            var $self = $(this), map = {
+                stu_id: /^[\d]{7}$/gi,
+                stu_pwd: /^[\s\S]{6,20}$/g,
+                stu_nickname: /^([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[\w])+$/gi
+            };
+            check.call($self, map) || ev.preventDefault();
+        }
+        function check(map) {
+            var $self = $(this), returnValue = !0, $input = $(".js-check", $self);
+            return $input.each(function($i, ele) {
+                var $ele = $(ele), key = $ele.attr("data-check_type");
+                map[key] && "[object RegExp]" == Object.prototype.toString.call(map[key]) && (map[key].test($ele.val()) ? $ele.next(".s-error").css("display", "none") : ($ele.next(".s-error").css("display", "block"), 
+                returnValue = !1));
+            }), console.log(returnValue), returnValue;
+        }
+        /*阻止事件冒泡*/
+        $(".m-login-register-case").on("click", function(ev) {
+            ev.stopPropagation();
+        }), /*tab切换*/
+        openBox($(".js-open-register"), $(".u-register")), openBox($(".js-open-login"), $(".u-login")), 
+        /*表单提交*/
+        $(".js-login-form, .js-register-form").on("submit", checkForm);
+    });
+});
