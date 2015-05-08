a5b26f472b4dfd8ddb8976c784965d4871d538db
Lich <longzy2013@gmail.com>
2015-05-09 00:48:13 +0800

    cut head

--- app/controllers/PersonalController.php
+++ app/controllers/PersonalController.php
@@ -84,8 +84,8 @@ class PersonalController extends BaseController {
     //裁剪头像
     public function uploadHeadCut() {
         $data = Input::all();
-        $data['width'] = $data['width'] > 400? $data['width'] : 400;
-        $data['height'] = $data['height'] > 400? $data['height'] : 400;
+        $data['width'] = $data['width'] < 400? $data['width'] : 400;
+        $data['height'] = $data['height'] < 400? $data['height'] : 400;
         $img = Image::make(Session::get('headpath'));
         $img->crop($data['width'], $data['height'], $data['x1'], $data['y1']);
         $img->save(Session::get('headpath'));
