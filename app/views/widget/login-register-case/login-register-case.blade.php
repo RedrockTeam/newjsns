36a589a025379002dc369575e8f8caa2d0eafaaa
lijinxin <840277005@qq.com>
2015-05-24 22:48:33 +0800

    '改变登陆方式'

--- app/views/widget/login-register-case/login-register-case.blade.php
+++ app/views/widget/login-register-case/login-register-case.blade.php
@@ -18,10 +18,10 @@
     {{--登陆 默认关闭--}}
     <form action="{{route('home/login')}}" novalidate method="POST"  class="u-login s-form f-cb js-login-form">
 
-        <div class="f-pr"><input type="text" class="u-stu_id js-check" name="username" data-check_type="stu_id"  placeholder="统一认证码"/><span class="s-error">+</span></div>
-        <div class="f-pr"><input type="password" class="u-stu_pwd js-check" name="password" data-check_type="stu_pwd" placeholder="密码（身份证后六位或者已经修改过的密码）"/><span class="s-error">+</span></div>
+        <div class="f-pr"><input type="text" class="u-stu_id js-check" name="username" data-check_type="stu_id"  placeholder="重邮通行证"/><span class="s-error">+</span></div>
+        <div class="f-pr"><input type="password" class="u-stu_pwd js-check" name="password" data-check_type="stu_pwd" placeholder="密码（你懂得!!!!）"/><span class="s-error">+</span></div>
         <input type="submit" value="登陆"/>
-        <a href="http://qxgl.cqupt.edu.cn/e2qPortalPub/security/user/userpwdrest.html" class="s-find_pwd">找回密码</a>
+        <a href="http://hongyan.cqupt.edu.cn/user/resetpassword" class="s-find_pwd">找回密码</a>
     </form>
     {{--注册 默认打开--}}
     <form action="{{route('home/register')}}" novalidate method="POST" class="u-register s-form f-cb js-login-form">
