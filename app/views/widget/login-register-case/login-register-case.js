f06ac53aada19436c508db3fd19b5e455f947f3d
lijinxin <840277005@qq.com>
2015-05-24 23:24:12 +0800

    'fix'

--- app/views/widget/login-register-case/login-register-case.js
+++ app/views/widget/login-register-case/login-register-case.js
@@ -28,7 +28,7 @@ define(['jquery', 'port'], function($, port){
             var $self = $(this),
                 map = {
                     'stu_id' : /^.+$/gi,
-                    'stu_pwd' : /^[\s\S]{6,20}$/g,
+                    'stu_pwd' : /^.+$/gi,
                     'stu_nickname' : /^([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[\w])+$/gi
                 };
             if( !check.call($self,map) ){
--- public/js/widget/login-register-case/login-register-case.js
+++ public/js/widget/login-register-case/login-register-case.js
@@ -6,7 +6,7 @@ define([ "jquery", "port" ], function($, port) {
         function checkForm(ev) {
             var $self = $(this), map = {
                 stu_id: /^.+$/gi,
-                stu_pwd: /^[\s\S]{6,20}$/g,
+                stu_pwd: /^.+$/gi,
                 stu_nickname: /^([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[\w])+$/gi
             };
             check.call($self, map) || ev.preventDefault();
