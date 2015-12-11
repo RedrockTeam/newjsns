fa2584e201faa0903d8f61c41ad7af2f3498287e
Lich2013 <longzy2013@gmail.com>
2015-12-11 16:36:23 +0800

    test

--- app/views/widget/litera_recom_slider/litera_recom_slider.blade.php
+++ app/views/widget/litera_recom_slider/litera_recom_slider.blade.php
@@ -2,7 +2,7 @@
     <ul class="items">
         @for($i = 0; $i < 4; ++$i)
             @if(isset($data['recommend_list'][$i]))
-        <li><a href="litera_sub?passage_id={{$data['recommend_list'][$i]['id']}}&type_id={{$data['recommend_list'][$i]['type_id']}}" title="{{$data['recommend_list'][$i]['title']}}"></a><img src="{{$data['recommend_list'][$i]['cover']}}" alt="" class="s-img"/></li>
+        <li><a href="litera_sub?passage_id={{$data['recommend_list'][$i]['id']}}&type_id={{$data['recommend_list'][$i]['type_id']}}" title="{{$data['recommend_list'][$i]['title']}}"></a><img src="{{substr($data['recommend_list'][$i]['cover'], 7)}}" alt="" class="s-img"/></li>
        @else
             @endif
         @endfor
