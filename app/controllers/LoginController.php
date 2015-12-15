8cf646c9cd30defffef3ba91c54662c84030379c
Lich2013 <longzy2013@gmail.com>
2015-12-15 15:06:15 +0800

    debug

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -22,6 +22,7 @@ class LoginController extends BaseController
             return Redirect::back()->withErrors($error, 'register');
         }
 //        $result = $this->get_register($input['username'], $input['password']);
+        http://hongyan.cqupt.edu.cn/RedCenter/Api/Handle/login
         $result = file_get_contents("http://hongyan.cqupt.edu.cn/online/interface.php?username=$input[username]&password=$input[password]");
         if ($result >0) {
             $num = User::where('uid', '=', $input['username'])->count();
@@ -54,8 +55,8 @@ class LoginController extends BaseController
          $num = User::where('uid', '=', $input['username'])->count();
          if($num > 0) {
 //              $result = $this->get_register($input['username'], $input['password']);
-                $result = file_get_contents("http://hongyan.cqupt.edu.cn/online/interface.php?username=$input[username]&password=$input[password]");
-             if ($result>0) {
+                $result = $this->__CurlPost(['user' => $input['username'], 'password' => $input['password']], 'http://hongyan.cqupt.edu.cn/RedCenter/Api/Handle/login');
+             if ($result['status'] == 200) {
                  if ($this->verify($input['username'], $input['username'])) {
                      $nickname = User::where('uid', '=', $input['username'])->first();
                      Session::put('nickname', $nickname['username']);
@@ -159,5 +160,27 @@ class LoginController extends BaseController
         return Redirect::to('/');
 
     }
+
+
+    /**
+     * 通过post方式获取数据, 支持https, 未测试233
+     * @param string $url
+     * @param array $data
+     * @return array|mixed
+     */
+    private function __CurlPost($url, $data = array()){
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        //运行curl，结果以json形式返回
+        $res = curl_exec($ch);
+        curl_close($ch);
+        //取出openid
+        return json_decode($res, true);
+    }
+
 
 }
